774b17ee1c6ced06436fc3de34c6d76d7b8bbdfc
Alex <alexbachurin91@gmail.com>
2021-09-26 15:20:37 +0300

    setup app html/css structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <div className="section-title">
+        <h1>Pagination</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div className="logo-container">
+                <img className="nav-logo" src="https://res.cloudinary.com/dljezd6qv/image/upload/v1632485129/logo-crop_dpqp9u.png" alt="bacha coding" />
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
